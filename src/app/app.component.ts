6edadd58a44f6c99bebb99615f286a2b53f88e73
Luiz Marinelli <luizh0508@gmail.com>
2021-03-25 11:21:02 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todolist';
+
+  newItem: string = '';
+
+  toDoList = ["Levar o lixo", "Lavar a louça", "Reunião do trabalho"];
+
+  addNewItem(){
+    this.toDoList.push(this.newItem);
+    this.newItem = '';
+
+    this.saveList();
+  }
+
+  removeFromList(index: number){
+    this.toDoList.splice(index, 1);
+    
+    this.saveList();
+  }
+
+  saveList(){
+    console.log(this.toDoList);
+  }
 }
